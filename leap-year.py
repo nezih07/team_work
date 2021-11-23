b7a8f9c6f29bc4be6c79bfb22e373ef8da9bdfed
nezih07 <94112386+nezih07@users.noreply.github.com>
2021-11-23 18:45:09 +0100

    Update leap-year.py
    I used yıl instead of year

--- leap-year.py
+++ leap-year.py
@@ -1,14 +1,14 @@
 # To get year (integer input) from the user
 # This is my first explanation
-year = int(input("Enter a year: "))
+yıl = int(input("Enter a year: "))
 
-if (year % 4) == 0:
+if (yıl % 4) == 0:
     if (year % 100) == 0:
-        if (year % 400) == 0:
-            print("{0} is a leap year".format(year))
+        if (yıl % 400) == 0:
+            print("{0} is a leap year".format(yıl))
         else:
-            print("{0} is not a leap year".format(year))
+            print("{0} is not a leap year".format(yıl))
     else:
-        print("{0} is a leap year".format(year))
+        print("{0} is a leap year".format(yıl))
 else:
-    print("{0} is not a leap year".format(year))
+    print("{0} is not a leap year".format(yıl))
